26a2d63d3211257106445b5fb754e6a092658a14
shivadxt <shivamweb3.0@gmail.com>
2023-12-22 18:36:16 +0530

    Flash Message Commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+//Creating Flash Message 
+router.get('/flash', function(req, res) {
+  req.flash("age",25);
+  req.flash("Name","ShivamSharma");
+  res.send("Flash Message Created !");
+});
+
+//Reading Flash Messages
+router.get('/flashread', function(req, res) {
+  console.log(req.flash("age"),req.flash("Name"));
+  res.send("Read FLASH MSG on console !");
 });
 
 module.exports = router;
